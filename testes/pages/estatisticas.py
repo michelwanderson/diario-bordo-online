bbfde333f0d8ddffd1fc0f29236319ffa06fb824
Wanderson Michel <wandersonmichel@MacBook-Air-de-Wanderson.local>
2025-11-14 16:50:01 -0300

    Update

--- testes/pages/estatisticas.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import streamlit as st
-
-
-
-st.markdown("<h2 style='text-align: center; color: black;'>Estatísticas de Bordo 📈</h2>", unsafe_allow_html=True)
-st.markdown("<h3 style='text-align: center; color: black;'>Em Desenvolvimento 🔨</h3>", unsafe_allow_html=True)
